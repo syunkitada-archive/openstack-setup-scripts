d9932e2009d4262ea157692d28456da2a5075086
owner <owner@localhost.openstack>
2014-08-24 22:33:58 +0900

    updated

--- setup_common.sh
+++ setup_common.sh
@@ -6,9 +6,17 @@
 
 source ./openstackrc
 
-# install epel repo
+# install icehouse and epel repo
+sudo yum install http://repos.fedorapeople.org/repos/openstack/openstack-icehouse/rdo-release-icehouse-3.noarch.rpm
 sudo yum install http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm -y
 sudo yum install openstack-utils -y
+
+# install pip
+sudo yum install python-devel libxml2-devel libxslt-devel -y
+wget http://python-distribute.org/distribute_setup.py
+sudo python distribute_setup.py
+sudo easy_install pip
+rm -f distribute*
 
 # setup ntp
 sudo yum install ntp -y
--- setup_keystone.sh
+++ setup_keystone.sh
@@ -4,7 +4,41 @@ source ./openstackrc
 
 sudo yum install openstack-keystone -y
 
+# setup db
+# sqlalchemyのバージョンを上げる
+# from sqlalchemy import exceptions as sa_exceptions
+#ImportError: cannot import name exceptions
+sudo pip install sqlalchemy --upgrade
+sudo pip install sqlalchemy-migrate --upgrade
 sudo openstack-config --set /etc/keystone/keystone.conf \
-   database connection mysql://$OS_DBUSER:$OS_DBPASS@$OS_DBHOST
+    database connection mysql://$OS_DBUSER:$OS_DBPASS@$OS_DBHOST/keystone
+sudo keystone-manage db_sync
 
-sudo pip install 'sqlalchemy <= 0.7.10'
+sudo openstack-config --set /etc/keystone/keystone.conf \
+    DEFAULT admin_token $OS_SERVICE_TOKEN
+
+sudo openstack-config --set /etc/keystone/keystone.conf \
+    token provider keystone.token.providers.uuid.Provider
+
+sudo service openstack-keystone start
+sudo chkconfig openstack-keystone on
+
+
+# create admin user
+keystone role-create --name=admin
+keystone user-create --name=admin --pass=$OS_ADMIN_PASS --email=OS_ADMIN_EMAIL
+keystone tenant-create --name=admin --description="Admin Tenant"
+keystone user-role-add --user=admin --tenant=admin --role=admin
+keystone user-role-add --user=admin --tenant=admin --role=_member_
+
+
+# difine services and api endpoints
+keystone service-create --name=keystone --type=identity \
+    --description="OpenStack Identity"
+keystone endpoint-create \
+    --service-id=$(keystone service-list | awk '/ identity / {print $2}') \
+    --publicurl=http://${OS_CTL_HOST}:5000/v2.0 \
+    --internalurl=http://${OS_CTL_HOST}:5000/v2.0 \
+    --adminurl=http://${OS_CTL_HOST}:35357/v2.0
+
+
